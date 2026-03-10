d471bdb25cb9cae263ee8aab7bcab7e8ea326cc1
Deepak yadav <deepakcodes18@gmail.com>
2026-03-10 19:28:34 +0530

    Updated Simon Game code

--- working.js
+++ working.js
@@ -54,7 +54,7 @@ function checkAns(idx){
             setTimeout(levelUp,1000)
         }
     } else{
-        h2.innerHTML=`Game Over BSDK! Your score was <b>${level}</b> <br> Press any key to start.`;
+        h2.innerHTML=`Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
         document.querySelector("body").style.backgroundColor ="red";
         setTimeout(function(){
             document.querySelector("body").style.backgroundColor="white";
